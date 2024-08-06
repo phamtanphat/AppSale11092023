417860f13b0bd9647eff99fb62057279df7d653b
phatdroid94 <phatdroid94@gmail.com>
2024-08-06 21:23:47 +0700

    handle authorization

--- app/src/main/java/com/example/appsale11092023/common/AppCommon.kt
+++ app/src/main/java/com/example/appsale11092023/common/AppCommon.kt
@@ -6,4 +6,10 @@ object AppCommon {
 
     // Time
     val TIME_REQUEST_DEFAULT = 30L
+
+    // File name shared preferences
+    val FILE_NAME_SHARED_PREFERENCES = "my-shared-preferences"
+
+    // Key shared preferences
+    val KEY_TOKEN = "token"
 }
